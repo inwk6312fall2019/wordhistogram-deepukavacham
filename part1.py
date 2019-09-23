d9d4983dd8c126546bf28fddc3e76fe7dafb94db
akhila1810 <ak782010@dal.ca>
2019-09-23 04:56:35 -0700

    part1

---
+++ part1.py
@@ -0,0 +1,10 @@
+my_files=open("2600.0.txt")
+def dup(my_file):
+	for word in my file(0,len(words)):
+		word=word.split()
+	if word==word:
+		print("the word is duplicate")
+	else:
+		print("the word is not duplicate")
+dup(my_file)
+
